8a12216dfb539d32f5b5d259ee0dd11fffe9f75d
Ming-der Wang <47238+mingderwang@users.noreply.github.com>
2021-07-13 11:57:03 +0800

    Initial commit

---
+++ src/screens/complianceScreen.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+
+//IMPORTING STYLESHEET
+
+import "../styles/screens/withdraw.scss";
+
+//IMPORTING PATTERNS
+
+import { Footer, Wallet, Work } from "../patterns";
+import { Button } from "../components";
+import { getFullInfo } from "../utils/whirlwind";
+import { ProcessModal } from "../patterns/modals/modal";
+import ConnectModal from "../patterns/modals/connectModal";
+
+//IMPORTING MEDIA ASSETS
+
+import info from "../assets/icons/info.svg";
+import hint from "../assets/icons/hint.svg";
+import uparrow from "../assets/icons/uparrow.svg";
+
+const ComplianceScreen = () => {
+  const [notes, setNotes] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isConnectPopup, setIsConnectPopup] = useState(false);
+
+  //HANDLING METHODS
+
+  const handleCompliance = async () => {
+    setIsLoading(true);
+    await getFullInfo().catch((res) => {
+      alert(res);
+      setIsLoading(false);
+    });
+  };
+
+  //INLINE STYLES
+
+  const inlineStyle = {
+    flex: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      width: "100%",
+    },
+    flexGap: {
+      display: "flex",
+      alignItems: "center",
+      gridGap: 10,
+    },
+  };
+
+  const renderInfo = (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gridGap: 16,
+        marginBottom: "2em",
+      }}
+    >
+      <img src={info} alt="info" />
+      <p className="txt-reg-14-txt-grad">
+        This is an experimental software. Use it at your own risk.
+      </p>
+    </div>
+  );
+
+  // const renderDetailsCard = (
+  //   <div className="block-right">
+  //     <div style={inlineStyle.flex}>
+  //       <p className="title-b-24-txt-pri">Latest deposits</p>
+  //       <p className="txt-reg-14-txt-grad">see more</p>
+  //     </div>
+  //     <div className="transaction-card">
+  //       <div className="history">
+  //         <img src={uparrow} alt="uparrow" />
+  //         <span>0 ETH</span>
+  //         <span>address</span>
+  //         <span>2 mins</span>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
+  const renderDetails = (
+    <div className="details-block">
+      <div className="block-left">
+        <p style={inlineStyle.flexGap}>
+          <span className="txt-reg-16-txt-pri">Note</span>
+          <img src={info} alt="info" />
+        </p>
+        <div className="txt-reg-14-txt-pri note">
+          <input type="text" onChange={(e) => setNotes(e.target.value)} />
+        </div>
+        <p style={inlineStyle.flexGap}>
+          <span className="txt-reg-16-txt-pri">Recipient</span>
+          <img src={info} alt="info" />
+        </p>
+        <div style={inlineStyle.flexGap}>
+          <img src={hint} alt="hint" />
+          <p style={{ width: "90%" }} className="txt-reg-14-txt-sec">
+            <span style={{ color: "#4FACFE", fontSize: 16 }}>Hint:</span>
+            It is recommended you wait a little between deposit and withdrawal.
+            Give your deposit some time to mix with deposits from other users,
+            so that it won’t be possible to trace your transaction back to you.
+          </p>
+        </div>
+        {window.from ? (
+          <Button className="btn-primary" onClick={() => handleCompliance()}>
+            Get compliance info
+          </Button>
+        ) : (
+          <Button
+            className="btn-primary"
+            onClick={() => setIsConnectPopup(true)}
+          >
+            Connect wallet
+          </Button>
+        )}
+      </div>
+      {/* {renderDetailsCard} */}
+    </div>
+  );
+
+  const renderScreen = (
+    <div className="compliance-screen">
+      {renderInfo}
+      <Wallet
+        title="compliance"
+        details="Compliance is used to get info about a note, 
+                such as its deposit as well as any matching withdrawal."
+      />
+      {renderDetails}
+    </div>
+  );
+
+  return (
+    <>
+      {renderScreen}
+      {isLoading ? <ProcessModal variant="compliance" /> : null}
+      {isLoading || isConnectPopup ? (
+        <div
+          className="backdrop"
+          onClick={() => setIsConnectPopup(false)}
+        ></div>
+      ) : null}
+      {isConnectPopup ? (
+        <ConnectModal setIsConnectPopup={setIsConnectPopup} />
+      ) : null}
+      <Work />
+      <Footer />
+    </>
+  );
+};
+
+//EXPORTING SCREENS
+
+export default ComplianceScreen;
---
+++ src/screens/homeScreen.js
@@ -0,0 +1,432 @@
+import React, { useState } from "react";
+
+//IMPORTING STYLESHEET
+
+import "../styles/screens/home.scss";
+
+//IMPORTING PATTERNS
+
+import { NoteModal, SuccessModal } from "../patterns/modals/modal";
+import { Footer, Wallet, Work } from "../patterns/";
+import { Button } from "../components";
+import { deposit, getNote } from "../utils/whirlwind";
+import ConnectModal from "../patterns/modals/connectModal";
+
+//IMPORTING MEDIA ASSETS
+
+import info from "../assets/icons/info.svg";
+import hint from "../assets/icons/hint.svg";
+// import downarrow from "../assets/icons/downarrow.svg";
+
+const HomeScreen = () => {
+  // INITIALIZING HOOKS
+
+  const [notes, setNotes] = useState();
+  const [isSelectedBnb, setIsSelectedBnb] = useState(false);
+  const [isSelectedWind, setIsSelectedWind] = useState(false);
+  const [selectedCoin, setSelectedCoin] = useState();
+  const [isSelectedAmt, setIsSelectedAmt] = useState();
+  const [isNoteModal, setIsNoteModal] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIssuccess] = useState(false);
+  const [tab1, setTab1] = useState();
+  const [tab2, setTab2] = useState();
+  const [isConnectPopup, setIsConnectPopup] = useState(false);
+
+  // const handleDeposit = () => {
+  //   console.warn("coin", coin);
+  //   console.warn("amount", amount);
+  //   let notes = "Initial dummy note";
+  //   // web3 generating notes
+
+  //   setNotes(notes);
+  // };
+
+  //HANDLING DEPOSIT METHODS
+
+  async function proceedDeposit() {
+    if (notes !== undefined) {
+      setIsLoading(true);
+      try {
+        await deposit()
+          .then((res) => {
+            console.log(res);
+            setIsLoading(false);
+            setIsNoteModal(false);
+            setIssuccess(true);
+          })
+          .catch((err) => {
+            console.log(err);
+            setIsLoading(false);
+            setIsError(true);
+            setTimeout(() => {
+              setIsError(false);
+              setIsNoteModal(false);
+            }, 3000);
+          });
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      alert("Generate Notes before deposit");
+    }
+  }
+
+  async function setData() {
+    if (selectedCoin !== undefined || isSelectedAmt !== undefined) {
+      let note = getNote(selectedCoin, isSelectedAmt);
+      setNotes(note);
+      setIsNoteModal(true);
+    } else {
+      alert("Choose a coin and a amount for further proceeding");
+    }
+  }
+
+  const handleBnb = () => {
+    setIsSelectedWind(false);
+    setIsSelectedBnb(true);
+    setSelectedCoin(0);
+    setIsSelectedAmt();
+    setTab1();
+  };
+
+  const handleWind = () => {
+    setIsSelectedWind(true);
+    setIsSelectedBnb(false);
+    setSelectedCoin(1);
+    setIsSelectedAmt();
+    setTab2();
+  };
+
+  console.log(selectedCoin, "1");
+  console.log(isSelectedAmt);
+
+  const handleBnbAmt = (amt) => {
+    setIsSelectedAmt(amt);
+    setTab1(amt);
+  };
+
+  const handleWindAmt = (amt) => {
+    setIsSelectedAmt(amt);
+    setTab2(amt);
+  };
+
+  //INLINE STYLES
+
+  const inlineStyle = {
+    flex: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+      width: "100%",
+    },
+    flexGap: {
+      display: "flex",
+      alignItems: "center",
+      gridGap: 10,
+    },
+  };
+
+  //RENDER INFO
+
+  const renderInfo = (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gridGap: 16,
+        marginBottom: "2em",
+      }}
+    >
+      <img src={info} alt="info" />
+      <p className="txt-reg-14-txt-grad">
+        This is an experimental software. Use it at your own risk.
+      </p>
+    </div>
+  );
+
+  //RENDER DETAILS CARD
+
+  // const renderDetailsCard = (
+  //   <div className="block-right">
+  //     <div className="block-header">
+  //       <p className="title-b-24-txt-pri">Latest deposits</p>
+  //       <p className="txt-reg-14-txt-grad">see more</p>
+  //     </div>
+  //     <div className="transaction-card">
+  //       <div className="history">
+  //         <img src={downarrow} alt="downarrow" />
+  //         <span>0 ETH</span>
+  //         <span>address</span>
+  //         <span>2 mins ago</span>
+  //       </div>
+  //       <div className="history">
+  //         <img src={downarrow} alt="downarrow" />
+  //         <span>0 ETH</span>
+  //         <span>address</span>
+  //         <span>2 mins ago</span>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
+
+  //RENDER WIND AMOUNTS
+
+  const renderWINDAmounts = (
+    <>
+      <span
+        onClick={() => handleWindAmt(0)}
+        style={{
+          background:
+            tab2 === 0
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        1
+      </span>
+      <span
+        onClick={() => handleWindAmt(1)}
+        style={{
+          background:
+            tab2 === 1
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        10
+      </span>
+      <span
+        onClick={() => handleWindAmt(2)}
+        style={{
+          background:
+            tab2 === 2
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        100
+      </span>
+      <span
+        onClick={() => handleWindAmt(3)}
+        style={{
+          background:
+            tab2 === 3
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        1000
+      </span>
+    </>
+  );
+
+  //RENDER BNB AMOUNT
+
+  const renderBNBAmounts = (
+    <>
+      <span
+        onClick={() => handleBnbAmt(0)}
+        style={{
+          background:
+            tab1 === 0
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        0.1
+      </span>
+      <span
+        onClick={() => handleBnbAmt(1)}
+        style={{
+          background:
+            tab1 === 1
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        1
+      </span>
+      <span
+        onClick={() => handleBnbAmt(2)}
+        style={{
+          background:
+            tab1 === 2
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        10
+      </span>
+      <span
+        onClick={() => handleBnbAmt(3)}
+        style={{
+          background:
+            tab1 === 3
+              ? `linear-gradient(
+          180deg
+          , #00f2fe -24136%, #4facfe 48696%)`
+              : "",
+        }}
+      >
+        100
+      </span>
+    </>
+  );
+
+  //RENDER DETAILS
+
+  const renderDetails = (
+    <div className="details">
+      <div className="block-left">
+        <p className="txt-reg-16-txt-pri">Token </p>
+        <div style={inlineStyle.flex} className="select-input">
+          <div className="coin">
+            <p
+              className="title-b-16-txt-pri"
+              style={{
+                background: isSelectedBnb
+                  ? `linear-gradient(
+                  180deg
+                  , #00f2fe -24136%, #4facfe 48696%)`
+                  : null,
+              }}
+              onClick={() => handleBnb()}
+            >
+              Bnb
+            </p>
+            <p
+              className="title-b-16-txt-pri"
+              style={{
+                background: isSelectedWind
+                  ? `linear-gradient(
+                  180deg
+                  , #00f2fe -24136%, #4facfe 48696%)`
+                  : null,
+              }}
+              onClick={() => handleWind()}
+            >
+              Wind
+            </p>
+          </div>
+          <div style={inlineStyle.flexGap}>
+            <img src={info} alt="info" />
+            <span
+              className="txt-reg-14-txt-pri"
+              style={{ whiteSpace: "nowrap" }}
+            >
+              1 ETH = $663.37
+            </span>
+          </div>
+        </div>
+        <p
+          style={{
+            display: selectedCoin === undefined ? "none" : "flex",
+            gridGap: 10,
+            alignItems: "center",
+            opacity: selectedCoin === undefined ? "0" : "1",
+          }}
+        >
+          <span className="txt-reg-16-txt-pri">Amount</span>
+          <img src={info} alt="info" width={18} />
+        </p>
+        <div
+          className="amount-block"
+          style={{
+            display: selectedCoin === undefined ? "none" : "grid",
+            opacity: selectedCoin === undefined ? "0" : "1",
+          }}
+        >
+          {selectedCoin === 0 && renderBNBAmounts}
+          {selectedCoin === 1 && renderWINDAmounts}
+        </div>
+        <div style={inlineStyle.flexGap}>
+          <img src={hint} alt="hint" />
+          <p style={{ width: "90%" }} className="txt-reg-14-txt-sec">
+            <span style={{ color: "#4FACFE", fontSize: 16 }}>Hint:</span>
+            It is recommended you wait a little between deposit and withdrawal.
+            Give your deposit some time to mix with deposits from other users,
+            so that it won’t be possible to trace your transaction back to you.
+          </p>
+        </div>
+        {window.from ? (
+          <Button className="btn-primary" onClick={() => setData()}>
+            Generate note
+          </Button>
+        ) : (
+          <Button
+            className="btn-primary"
+            onClick={() => setIsConnectPopup(true)}
+          >
+            Connect wallet
+          </Button>
+        )}
+      </div>
+      {/* {renderDetailsCard} */}
+    </div>
+  );
+
+  //RENDER SCREEN
+
+  const renderHomeScreen = (
+    <div className="home">
+      {renderInfo}
+      <Wallet
+        title="deposit"
+        details="Depositing takes funds from your 
+                    wallet and enters them into the privacy 
+                    pool. Once there, they can be redeemed 
+                    by anyone with the note; a secret that 
+                    cannot be linked to the original deposit."
+      />
+      {renderDetails}
+    </div>
+  );
+
+  return (
+    <>
+      {renderHomeScreen}
+      {isNoteModal ? (
+        <NoteModal
+          note={notes}
+          setIsNoteModal={setIsNoteModal}
+          deposit={proceedDeposit}
+          isLoading={isLoading}
+          isError={isError}
+        />
+      ) : null}
+      {isNoteModal || isSuccess || isConnectPopup ? (
+        <div
+          className="backdrop"
+          onClick={() => setIsConnectPopup(false)}
+        ></div>
+      ) : null}
+      <SuccessModal setIssuccess={setIssuccess} isSuccess={isSuccess} />
+      {isConnectPopup ? (
+        <ConnectModal setIsConnectPopup={setIsConnectPopup} />
+      ) : null}
+      <Work />
+
+      <Footer />
+    </>
+  );
+};
+
+//EXPORTING SCREENS
+
+export default HomeScreen;
